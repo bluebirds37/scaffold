b40383c74122a4e59b353fa9279f93db520129f6
bluebirds37 <bluebirds37@163.com>
2019-10-10 09:22:27 +0800

    替换跨域解决方案

--- src/main/java/top/bluebirds37/scaffold/controller/UserController.java
+++ src/main/java/top/bluebirds37/scaffold/controller/UserController.java
@@ -3,8 +3,16 @@ package top.bluebirds37.scaffold.controller;
 
 import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.RequestMapping;
+import top.bluebirds37.scaffold.service.UserService;
+
+import javax.annotation.Resource;
 
 @RequestMapping("/user")
 @Api(tags = "001.用户")
 public class UserController {
+
+    @Resource
+    private UserService userService;
+
+
 }
